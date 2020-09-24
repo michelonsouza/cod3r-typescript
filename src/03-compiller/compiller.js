90bb3c4d791f27d95bddd8341e38333c1cb60943
Michelon Souza <michelon.souza@hotmail.com>
2020-09-24 01:30:50 -0300

    noEmitOnError

---
+++ src/03-compiller/compiller.js
@@ -0,0 +1,6 @@
+"use strict";
+var chanel = 'Gaveta';
+var subscribers = 610234;
+// chanel = subscribers;
+console.log("Chanel = " + chanel);
+// let name = 'pedro';
---
+++ src/03-compiller/compiller.ts
@@ -0,0 +1,7 @@
+const chanel = 'Gaveta';
+const subscribers = 610234;
+
+// chanel = subscribers;
+console.log(`Chanel = ${chanel}`);
+
+// let name = 'pedro';
