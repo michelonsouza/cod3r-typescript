a30c97c2dd1f9418bef9caed3a8a24695b78c95d
Michelon Souza <michelon.souza@hotmail.com>
2020-09-27 16:18:00 -0300

    spread & rest 3

--- src/04-ecmascript/ecmascript.ts
+++ src/04-ecmascript/ecmascript.ts
@@ -94,3 +94,18 @@ const sumVariableArgs = (...args: number[]): number => {
 };
 
 console.log(sumVariableArgs(...numbers));
+
+// rest & spread (Tupla)
+const tupla: [number, string, boolean] = [1, 'abc', false];
+
+function tuplaParam1(a: number, b: string, c: boolean): void {
+  console.log(`1) ${a} ${b} ${c}`);
+}
+
+tuplaParam1(...tupla);
+
+function tuplaParam2(...params: [number, string, boolean]): void {
+  console.log(`2) ${params[0]} ${params[1]} ${params[2]}`);
+}
+
+tuplaParam2(...tupla);
