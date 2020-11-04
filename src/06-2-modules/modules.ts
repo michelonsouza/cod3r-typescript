52b7d6a92efcbebb0be8eef9526fcdeaea566a6e
Michelon Souza <michelon.souza@hotmail.com>
2020-11-04 02:33:30 -0300

    use "commonJS" modules

--- src/06-2-modules/modules.ts
+++ src/06-2-modules/modules.ts
@@ -3,7 +3,11 @@ import { reactArea } from './rect';
 // import { circleArea } from './circle';
 import { circleArea as circ } from './circle';
 
+const { sayHello } = require('./new');
+
 console.log('Modulo carregado...');
 
 console.log(reactArea(7, 8));
 console.log(circ(2));
+
+console.log(sayHello('Michelon'));
