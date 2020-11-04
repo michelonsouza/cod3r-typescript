ffd8a30873b213eafdc6c949ec3ed76d042e9cf6
Michelon Souza <michelon.souza@hotmail.com>
2020-11-04 03:53:01 -0300

    interface prototype Object

--- src/07-interfaces/interfaces.ts
+++ src/07-interfaces/interfaces.ts
@@ -56,3 +56,89 @@ const power: CalcFunction = (base: number, exp: number): number => {
 };
 
 console.log(power(3, 10));
+
+// Heritage
+interface A {
+  a(): void;
+}
+
+interface B {
+  b(): void;
+}
+
+interface ABC extends A, B {
+  c(): void;
+}
+
+class RealA implements A {
+  a(): void {
+    console.log('method a');
+  }
+}
+
+class RealAB implements A, B {
+  a(): void {
+    console.log('method A');
+  }
+
+  b(): void {
+    console.log('method A');
+  }
+}
+
+class RealABC implements ABC {
+  a(): void {
+    console.log('method A');
+  }
+
+  b(): void {
+    console.log('method A');
+  }
+
+  c(): void {
+    console.log('method A');
+  }
+}
+
+function test(b: B): void {
+  console.log(b);
+}
+
+test(new RealABC());
+
+abstract class AbstractABD implements A, B {
+  a(): void {
+    console.log('method A');
+  }
+
+  b(): void {
+    console.log('method A');
+  }
+
+  abstract d(): void;
+}
+
+interface Object {
+  log(): void;
+}
+
+Object.prototype.log = function () {
+  console.log(this.toString());
+};
+
+const ax = 2;
+const ay = 3;
+const az = 4;
+
+const cli = {
+  name: 'Michelon',
+  toString() {
+    return this.name;
+  },
+};
+
+ax.log();
+ay.log();
+az.log();
+
+cli.log();
