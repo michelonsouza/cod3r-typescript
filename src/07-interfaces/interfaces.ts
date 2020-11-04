b0f65c10ca01806af3f1bb0051b51fdf80338dbc
Michelon Souza <michelon.souza@hotmail.com>
2020-11-04 03:24:36 -0300

    interface tytpe function

--- src/07-interfaces/interfaces.ts
+++ src/07-interfaces/interfaces.ts
@@ -26,3 +26,33 @@ changeName(person);
 sayHello(person);
 
 person.greeting('Skywalker');
+
+// Using Classes...
+
+class ClientHuman implements Human {
+  public name = '';
+
+  public lastBuy = new Date();
+
+  greeting(surname: string): void {
+    console.log(`Olá, meu nome é ${this.name} ${surname}`);
+  }
+}
+
+const clientHuman = new ClientHuman();
+clientHuman.name = 'Han';
+sayHello(clientHuman);
+clientHuman.greeting('Solo');
+console.log(clientHuman.lastBuy);
+
+// Interface function
+
+interface CalcFunction {
+  (a: number, b: number): number;
+}
+
+const power: CalcFunction = (base: number, exp: number): number => {
+  return base ** exp;
+};
+
+console.log(power(3, 10));
