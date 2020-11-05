8545e840b649e404d5d465a44db18ce45deeb29a
Michelon Souza <michelon.souza@hotmail.com>
2020-11-05 00:51:43 -0300

    using generics 2

--- src/08-generics/generics.ts
+++ src/08-generics/generics.ts
@@ -21,3 +21,26 @@ const tests: Array<number> = [10, 9.3, 7.7];
 tests.push(8.4);
 // tests.push('5.5');
 console.log(tests);
+
+// Array
+function printer<T>(args: T[]): void {
+  args.forEach(element => console.log(element));
+}
+
+printer([1, 2, 3]);
+printer<number>([1, 2, 3]);
+printer<string>(['michelon', 'lilian', 'julia']);
+printer<{ name: string; age: number }>([
+  { name: 'michelon', age: 36 },
+  { name: 'lilian', age: 32 },
+]);
+
+interface Student {
+  name: string;
+  age: number;
+}
+
+printer<Student>([
+  { name: 'michelon', age: 36 },
+  { name: 'lilian', age: 32 },
+]);
